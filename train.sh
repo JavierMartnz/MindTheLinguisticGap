6c929298c3e944877a936994425e3ef9d0545a9a
Javier Martinez <javimr98@gmail.com>
2022-04-19 20:07:24 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-source /vol/tensusers5/jmartinez/pretraini3d/bin/activate
 cd /vol/tensusers5/jmartinez/MindTheLinguisticGap
 CUDA_LAUNCH_BLOCKING=1 python ./src/train_i3d.py --config_path=/vol/tensusers5/jmartinez/MindTheLinguisticGap/config.yaml
-deactivate
