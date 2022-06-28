d7470df897b315dcc08a47fa77fd5cb973fe333f
Javier Martinez <javimr98@gmail.com>
2022-06-28 11:22:10 +0200

    Update ft_i3d.py

--- src/ft_i3d.py
+++ src/ft_i3d.py
@@ -182,7 +182,7 @@ def run(cfg_path, mode='rgb'):
                     # upsample to input size
                     per_frame_logits = F.upsample(per_frame_logits, t, mode='linear')
 
-                    print(per_frame_logits.size)
+                    print(per_frame_logits.size())
 
                     # compute localization loss
                     loc_loss = F.binary_cross_entropy_with_logits(per_frame_logits, labels)
