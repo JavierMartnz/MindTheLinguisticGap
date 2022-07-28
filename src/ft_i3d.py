d69e1fcb1065956bb628760d970d1aaf245657e6
Javier Martinez <javimr98@gmail.com>
2022-07-28 12:02:24 +0200

    Update ft_i3d.py

--- src/ft_i3d.py
+++ src/ft_i3d.py
@@ -52,13 +52,17 @@ def f1_score(TN, TP, FP, FN):
 
 def get_prediction_measures(labels, frame_logits):
 
+    labels = labels.detach().cpu().numpy()
+    frame_logits = frame_logits.detach().cpu().numpy()
+
+
     for batch in range(labels.size(0)):
         FP = 0
         FN = 0
         TP = 0
         TN = 0
         for frame in range(labels.size(2)):
-            confusion_matrix(labels.detach().cpu().numpy(), frame_logits.detach().cpu().numpy())
+            confusion_matrix(labels[batch, :, frame], frame_logits[batch, :, frame])
             f_FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)
             f_FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
             f_TP = np.diag(confusion_matrix)
