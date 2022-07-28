0a72c4433bd11e8d1911a21f303649df64e237c1
Javier Martinez <javimr98@gmail.com>
2022-07-28 16:06:58 +0200

    Update ft_i3d.py

--- src/ft_i3d.py
+++ src/ft_i3d.py
@@ -238,7 +238,7 @@ def run(cfg_path, mode='rgb'):
                     y_true = np.argmax(labels.detach().cpu().numpy(), axis=1)
 
                     batch_acc = [accuracy_score(y_true[i], y_pred[i]) for i in range(np.shape(y_pred)[0])]
-                    batch_f1 = [f1_loss(y_true[i], y_pred[i], average='macro') for i in range(np.shape(y_pred)[0])]
+                    batch_f1 = [f1_score(y_true[i], y_pred[i], average='macro') for i in range(np.shape(y_pred)[0])]
 
                     # b_TP, b_TN, b_FP, b_FN = get_prediction_measures(labels, per_frame_logits)
                     # batch_acc, batch_f1, _, _ = f1_score(b_TP, b_TN, b_FP, b_FN)
