2ca73c20a0d61d6017ba8676523b181dae77542f
Javier Martinez <javimr98@gmail.com>
2022-08-02 10:35:23 +0200

    Update ft_i3d.py

--- src/ft_i3d.py
+++ src/ft_i3d.py
@@ -171,7 +171,7 @@ def run(cfg_path, mode='rgb'):
 
     # unfreeze the ones we want
     i3d.logits.requires_grad_(True)
-    unfreeze_layers = ['Mixed_5c', 'Mixed_5b']
+    unfreeze_layers = ['Mixed_5c', 'Mixed_5b', 'MaxPool3d_5a_2x2', 'Mixed_4f', 'Mixed_4e', 'Mixed_4d', 'Mixed_4c', 'Mixed_4b']
     for layer in unfreeze_layers:
         i3d.end_points[layer].requires_grad_(True)
 
