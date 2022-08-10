6c1e2497b6676a85a1c931b66f75cef8e57304a8
Javier Martinez <javimr98@gmail.com>
2022-08-10 11:35:27 +0200

    Update ft.sh

--- ft.sh
+++ ft.sh
@@ -2,6 +2,5 @@
 
 source /vol/tensusers5/jmartinez/pretraini3d/bin/activate
 cd /vol/tensusers5/jmartinez/MindTheLinguisticGap
-tensorboard --logdir=runs --bind_all &
 python3 ./src/ft_i3d.py --config_path=/vol/tensusers5/jmartinez/MindTheLinguisticGap/config.yaml
 deactivate
