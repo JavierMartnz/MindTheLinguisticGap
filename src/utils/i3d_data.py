f5a48bdfa73095e7021a7e2d8e102d30d1e53661
Javier Martinez <javimr98@gmail.com>
2022-08-13 14:48:32 +0200

    Update i3d_data.py

--- src/utils/i3d_data.py
+++ src/utils/i3d_data.py
@@ -64,7 +64,7 @@ def load_rgb_frames(video_path, start_frame, window_size=64):
             img = cv2.resize(img, dsize=(0, 0), fx=sc, fy=sc)
         img = (img / 255.) * 2 - 1
         # change the img from (W, H, C) to (C, H, W) for the transformations
-        img = img.transpose(2, 1, 0)
+        img = img.transpose([2, 1, 0])
         frames.append(img)
 
     # now make sure that the corresponding number of windows is filled
@@ -74,7 +74,7 @@ def load_rgb_frames(video_path, start_frame, window_size=64):
         for i in range(start_frame + window_size - last_frame):
             frames.append(frames[i])
 
-    return torch.Tensor(frames[start_frame:start_frame + window_size])
+    return torch.Tensor(np.array(frames[start_frame:start_frame + window_size]))
 
 
 # def load_flow_frames(root, vid, start, num):
@@ -242,7 +242,7 @@ class I3Dataset(data_utl.Dataset):
             imgs = self.transforms(imgs)
 
         # change from (T, C, H, W)  to shape (C, T, H, W) for network input
-        imgs = imgs.transpose(1, 0, 2, 3)
+        imgs = imgs.transpose([1, 0, 2, 3])
 
         return video_to_tensor(imgs), torch.from_numpy(label)
 
