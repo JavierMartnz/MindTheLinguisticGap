79b1c5a930330e2787caa4033dda2d4e805e475e
Javier Martinez <javimr98@gmail.com>
2022-08-19 10:04:03 +0200

    Update test_i3d.py

--- src/test_i3d.py
+++ src/test_i3d.py
@@ -83,6 +83,9 @@ def test(cfg_path, mode="rgb"):
                     total_pred = np.append(total_pred, y_pred.flatten())
                     total_true = np.append(total_true, y_true.flatten())
 
+    print(total_pred)
+    print(total_true)
+
     cm = confusion_matrix(total_true, total_pred)
     f1 = f1_score(total_true, total_pred)
     acc = accuracy_score(total_true, total_pred)
