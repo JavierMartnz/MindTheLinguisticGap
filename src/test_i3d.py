4c205027af1d171c804703087c8df4e9797fd0fa
Javier Martinez <javimr98@gmail.com>
2022-08-19 09:50:18 +0200

    Update test_i3d.py

--- src/test_i3d.py
+++ src/test_i3d.py
@@ -82,8 +82,6 @@ def test(cfg_path, mode="rgb"):
                     total_pred = np.append(total_pred, y_pred, axis=0)
                     total_true = np.append(total_true, y_true, axis=0)
 
-                print(np.shape(total_pred), np.shape(y_pred))
-
                 # calculate batch metrics by averaging
                 # batch_acc = np.mean([accuracy_score(y_true[i], y_pred[i]) for i in range(np.shape(y_pred)[0])])
                 # batch_f1 = np.mean([f1_score(y_true[i], y_pred[i], average='macro') for i in range(np.shape(y_pred)[0])])
@@ -91,7 +89,10 @@ def test(cfg_path, mode="rgb"):
                 # acc_list.append(batch_acc)
                 # f1_list.append(batch_f1)
 
-    # print(np.shape(total_pred))
+    cm = confusion_matrix(y_true, y_pred)
+
+    print(cm)
+
 
 
 def main(params):
