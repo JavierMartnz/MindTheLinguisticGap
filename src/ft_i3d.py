e35ab3023645eb3d6afc7f8dbef70054411f0cfc
Javier Martinez <javimr98@gmail.com>
2022-10-11 21:15:46 +0200

    Update ft_i3d.py

--- src/ft_i3d.py
+++ src/ft_i3d.py
@@ -134,7 +134,7 @@ def run(cfg_path, mode='rgb'):
     val_transforms = transforms.Compose([transforms.CenterCrop(224)])
 
     num_top_glosses = None  # should be None if no filtering wanted
-    specific_glosses = ["PT-1hand", "PO"]
+    specific_glosses = ["JA-A", "GEBAREN-A"]
 
     # get glosses from the class encodings
     sb_vocab = load_gzip(sb_vocab_path)
