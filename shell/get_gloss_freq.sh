855b4972f811b39c034eb987ff1305f806baa0ea
Javier Martinez <javimr98@gmail.com>
2023-02-10 10:13:10 +0100

    Update get_gloss_freq.sh

--- shell/get_gloss_freq.sh
+++ shell/get_gloss_freq.sh
@@ -2,5 +2,5 @@
 
 source /vol/tensusers5/jmartinez/pretraini3d/bin/activate
 cd /vol/tensusers5/jmartinez/MindTheLinguisticGap
-python3 ./src/gloss_freq_cngt_clips.py --cngt_root=/vol/tensusers5/jmartinez/cngt_single_signs_512 --fig_output_root=/vol/tensusers5/jmartinez/graphs --max_glosses=10
+python3 ./src/gloss_freq_cngt_clips.py --cngt_root=/vol/tensusers5/jmartinez/datasets/cngt_single_signs_512 --fig_output_root=/vol/tensusers5/jmartinez/graphs --max_glosses=10
 deactivate
