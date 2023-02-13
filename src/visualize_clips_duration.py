1de5c752d915e7980c23641e7c1d97b9ae98302c
Javier Martinez <javimr98@gmail.com>
2023-02-13 16:20:52 +0100

    Update visualize_clips_duration.py

--- src/visualize_clips_duration.py
+++ src/visualize_clips_duration.py
@@ -10,31 +10,29 @@ from pathlib import Path
 import subprocess
 
 
-def print_stats(frame_durations: list, framerate: int, dataset: str, fig_output_root: str):
+def print_stats(clip_durations: list, framerate: int, dataset: str, fig_output_root: str):
     plt.style.use(Path(__file__).parent.resolve() / "../plot_style.txt")
 
-    upper_quartile = np.percentile(frame_durations, 75)
-    lower_quartile = np.percentile(frame_durations, 25)
+    print(clip_durations)
+
+    upper_quartile = np.percentile(clip_durations, 75)
+    lower_quartile = np.percentile(clip_durations, 25)
     iqr = upper_quartile - lower_quartile
 
-    frame_durations = np.array(frame_durations)
+    clip_durations = np.array(clip_durations)
 
-    upper_whisker = frame_durations[np.where(frame_durations <= upper_quartile + 1.5 * iqr, True, False)].max()
-    lower_whisker = frame_durations[np.where(frame_durations >= lower_quartile - 1.5 * iqr, True, False)].min()
+    upper_whisker = clip_durations[np.where(clip_durations <= upper_quartile + 1.5 * iqr, True, False)].max()
+    lower_whisker = clip_durations[np.where(clip_durations >= lower_quartile - 1.5 * iqr, True, False)].min()
 
     f, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.20, .80)})
 
-    ax_box.boxplot(frame_durations, showfliers=False, vert=False)
+    ax_box.boxplot(clip_durations, showfliers=False, vert=False)
     ax_box.set_yticks([])
 
-    ax_hist.hist(frame_durations, bins='auto', align='mid')
+    ax_hist.hist(clip_durations, bins='auto', align='mid')
+
     plt.xlim([lower_whisker - 1, upper_whisker + 1])
     plt.gca().set_xticks(np.linspace(lower_whisker - 1, upper_whisker + 1, num=8, dtype=int))
-
-    # plt.hist(frame_durations, bins='auto')
-    # plt.yscale('log')
-    # plt.axvline(mean, c='r', ls='--')
-    # plt.axvspan(mean - std, mean + std, alpha=0.2, color='red')
     plt.suptitle(f"Number of frames per clip in {dataset} at {framerate} fps")
     plt.ylabel("Number of clips")
     plt.xlabel("Number of frames")
@@ -47,34 +45,33 @@ def print_stats(frame_durations: list, framerate: int, dataset: str, fig_output_
 def get_stats_cngt(cngt_root: str, framerate: int, fig_output_root: str):
     cngt_clips = [file for file in os.listdir(cngt_root) if file.endswith('mpg') or file.endswith('.mov')]
 
-    frame_durations = []
+    clip_durations = []
     for clip in tqdm(cngt_clips):
         start_ms = int(clip.split("_")[4])
         end_ms = int(clip.split("_")[5])
-        gloss = clip.split("_")[-2]
-        duration_frames = math.ceil(framerate * (end_ms - start_ms) / 1000)
-        frame_durations.append(duration_frames)
+        n_frames = math.ceil(framerate * (end_ms - start_ms) / 1000)
+        clip_durations.append(n_frames)
 
-    print_stats(frame_durations, framerate, "CNGT", fig_output_root)
+    print_stats(clip_durations, clip_durations, "CNGT", fig_output_root)
 
 
 def get_stats_signbank(signbank_root: str, framerate: int, fig_output_root: str):
     sb_clip_paths = [os.path.join(signbank_root, file) for file in os.listdir(signbank_root) if
                      file.endswith('mp4') or file.endswith('.mov')]
 
-    frame_durations = []
+    clip_durations = []
     for clip in tqdm(sb_clip_paths):
         vcap = cv2.VideoCapture(clip)
-        frames = 0
+        n_frames = 0
         while True:
             success, _ = vcap.read()
             if not success:
                 break
-            frames += 1
+            n_frames += 1
 
-        frame_durations.append(frames)
+        clip_durations.append(n_frames)
 
-    print_stats(frame_durations, framerate, "Signbank", fig_output_root)
+    print_stats(clip_durations, framerate, "Signbank", fig_output_root)
 
 
 def main(params):
