f5d0458fc81255ea0de771da686ce6e50e9e042f
Javier Martinez <javimr98@gmail.com>
2023-02-13 16:10:44 +0100

    Update visualize_clip_duration.sh

--- shell/visualize_clip_duration.sh
+++ shell/visualize_clip_duration.sh
@@ -2,5 +2,5 @@
 
 source /vol/tensusers5/jmartinez/pretraini3d/bin/activate
 cd /vol/tensusers5/jmartinez/MindTheLinguisticGap
-python3 ./src/gloss_freq_cngt_clips.py --root=/vol/tensusers5/jmartinez/datasets --cngt_folder=cngt_single_signs_12fps --sb_folder=NGT_Signbank_12fps --fig_output_root=/vol/tensusers5/jmartinez/graphs --framerate=12
+python3 ./src/visualize_clips_duration.py --root=/vol/tensusers5/jmartinez/datasets --cngt_folder=cngt_single_signs_12fps --sb_folder=NGT_Signbank_12fps --fig_output_root=/vol/tensusers5/jmartinez/graphs --framerate=12
 deactivate
