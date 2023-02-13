66cb1d3426add48fe113f956a6718d2c4fa051a4
Javier Martinez <javimr98@gmail.com>
2023-02-13 20:36:35 +0100

    Update trim_signs_cngt.py

--- src/trim_signs_cngt.py
+++ src/trim_signs_cngt.py
@@ -71,132 +71,131 @@ def trim_clip(input_filename, start_time, end_time, start_frame, end_frame, glos
     return output_filename
 
 
-def process_file_for_trimming(file, cngt_root, cngt_output_root, signbank_vocab_path, window_size):
-    if file.endswith('.mpg') or file.endswith('.mov'):
-        file_path = os.path.join(cngt_root, file)
-        ann_path = file_path[:-3] + 'eaf'
+def process_file_for_trimming(file, cngt_root, cngt_output_root, signbank_vocab_path, framerate, window_size):
+    file_path = os.path.join(cngt_root, file)
+    ann_path = file_path[:-3] + 'eaf'
 
-        # check if the associated annotation file exists
-        if not os.path.exists(ann_path):
-            print(f"Early return: video {file} does not have an associated annotation file")
-            return
+    # check if the associated annotation file exists
+    if not os.path.exists(ann_path):
+        print(f"Early return: video {file} does not have an associated annotation file")
+        return
 
-        vcap = cv2.VideoCapture(file_path)
+    vcap = cv2.VideoCapture(file_path)
 
-        num_video_frames = vcap.get(cv2.CAP_PROP_FRAME_COUNT)
+    num_video_frames = vcap.get(cv2.CAP_PROP_FRAME_COUNT)
 
-        if num_video_frames <= 0:
-            print('Early return: video has zero frames')
-            return
+    if num_video_frames <= 0:
+        print('Early return: video has zero frames')
+        return
 
-        sb_vocab = load_gzip(signbank_vocab_path)
-        ann_file = pympi.Elan.Eaf(ann_path)
+    sb_vocab = load_gzip(signbank_vocab_path)
+    ann_file = pympi.Elan.Eaf(ann_path)
 
-        glosses_lefth = ann_file.get_annotation_data_for_tier(list(ann_file.tiers.keys())[0])
-        glosses_righth = ann_file.get_annotation_data_for_tier(list(ann_file.tiers.keys())[1])
+    glosses_lefth = ann_file.get_annotation_data_for_tier(list(ann_file.tiers.keys())[0])
+    glosses_righth = ann_file.get_annotation_data_for_tier(list(ann_file.tiers.keys())[1])
 
-        right_intervalTree = IntervalTree()
+    right_intervalTree = IntervalTree()
 
-        if glosses_righth:
+    if glosses_righth:
 
-            for ann in glosses_righth:
-                start_ms, stop_ms = ann[0], ann[1]
-                gloss = ann[2]
+        for ann in glosses_righth:
+            start_ms, stop_ms = ann[0], ann[1]
+            gloss = ann[2]
 
-                # this line unifies glosses to Dutch
-                parsed_gloss = parse_cngt_gloss(gloss, sb_vocab)
+            # this line unifies glosses to Dutch
+            parsed_gloss = parse_cngt_gloss(gloss, sb_vocab)
 
-                start_frame = math.ceil(25.0 * (start_ms / 1000.0))
-                stop_frame = math.floor(25.0 * (stop_ms / 1000.0)) + 1
+            start_frame = math.ceil(framerate * (start_ms / 1000.0))
+            stop_frame = math.floor(framerate * (stop_ms / 1000.0)) + 1
 
-                if start_frame > num_video_frames or stop_frame > num_video_frames:
+            if start_frame > num_video_frames or stop_frame > num_video_frames:
+                return
+
+            data = {'parsed_gloss': parsed_gloss, 'start_ms': start_ms, 'stop_ms': stop_ms}
+
+            interval = Interval(begin=start_ms, end=stop_ms, data=data)
+
+            if parsed_gloss in sb_vocab['gloss_to_id']:
+                right_intervalTree.add(interval)
+
+    merged_intervalTree = right_intervalTree.copy()
+
+    if glosses_lefth:
+
+        for ann in glosses_lefth:
+            start_ms, stop_ms = ann[0], ann[1]
+            gloss = ann[2]
+            parsed_gloss = parse_cngt_gloss(gloss, sb_vocab)
+            start_frame = math.ceil(framerate * (start_ms / 1000.0))
+            stop_frame = math.floor(framerate * (stop_ms / 1000.0)) + 1
+
+            if start_frame > num_video_frames or stop_frame > num_video_frames:
+                return
+
+            begin = start_frame
+            end = stop_frame
+
+            # these code section stops duplicated annotation of glosses in different hands
+            overlaps = right_intervalTree.overlap(begin, end)
+            if overlaps:
+                overlap_exceeded = False
+                for interval in overlaps:
+                    intersection = min(end, interval.end) - max(begin, interval.begin)
+                    union = max(end, interval.end) - min(begin, interval.begin)
+                    iou = intersection / union
+                    # if the gloss overlaps a lot with one in the intervalTree, skip gloss
+                    if iou >= 0.9:
+                        overlap_exceeded = True
+                        break
+                if overlap_exceeded:
                     return
 
-                data = {'parsed_gloss': parsed_gloss, 'start_ms': start_ms, 'stop_ms': stop_ms}
+            if parsed_gloss in sb_vocab['gloss_to_id']:
+                for interval in right_intervalTree:
 
-                interval = Interval(begin=start_ms, end=stop_ms, data=data)
+                    data = {'parsed_gloss': parsed_gloss}
 
-                if parsed_gloss in sb_vocab['gloss_to_id']:
-                    right_intervalTree.add(interval)
+                    if begin == interval.end and interval.data['parsed_gloss'] == parsed_gloss:
+                        data['start_ms'] = interval.data['start_ms']
+                        data['stop_ms'] = stop_ms
+                        merged_interval = Interval(interval.begin, end, data)
+                        merged_intervalTree.remove(interval)
+                        merged_intervalTree.add(merged_interval)
 
-        merged_intervalTree = right_intervalTree.copy()
+                    if end == interval.begin and interval.data == parsed_gloss:
+                        data['start_ms'] = start_ms
+                        data['stop_ms'] = interval.data['stop_ms']
+                        merged_interval = Interval(begin, interval.end, data)
+                        merged_intervalTree.remove(interval)
+                        merged_intervalTree.add(merged_interval)
 
-        if glosses_lefth:
+    clip_cnt = 0
 
-            for ann in glosses_lefth:
-                start_ms, stop_ms = ann[0], ann[1]
-                gloss = ann[2]
-                parsed_gloss = parse_cngt_gloss(gloss, sb_vocab)
-                start_frame = math.ceil(25.0 * (start_ms / 1000.0))
-                stop_frame = math.floor(25.0 * (stop_ms / 1000.0)) + 1
+    for interval_obj in merged_intervalTree:
+        trimmed_filename = trim_clip(file_path,
+                                     interval_obj.data['start_ms'],
+                                     interval_obj.data['stop_ms'],
+                                     interval_obj.begin,
+                                     interval_obj.end,
+                                     interval_obj.data['parsed_gloss'],
+                                     sb_vocab['gloss_to_id'][interval_obj.data['parsed_gloss']],
+                                     cngt_output_root)
 
-                if start_frame > num_video_frames or stop_frame > num_video_frames:
-                    return
+        # if a trimmed file was created, save the corresponding metadata
+        if trimmed_filename is not None:
+            # since opencv's number of frames is unreliable, we count the frames ourselves
+            num_trimmed_frames = count_video_frames(trimmed_filename)
+            # we create metadata that will be helpful for the loading
+            metadata = {"num_frames": num_trimmed_frames, "start_frames": []}
+            num_clips = math.ceil(num_trimmed_frames / window_size)
+            for j in range(num_clips):
+                metadata["start_frames"].append(j * window_size)
 
-                begin = start_frame
-                end = stop_frame
+            save_gzip(metadata, trimmed_filename[:-3] + 'gzip')
 
-                # these code section stops duplicated annotation of glosses in different hands
-                overlaps = right_intervalTree.overlap(begin, end)
-                if overlaps:
-                    overlap_exceeded = False
-                    for interval in overlaps:
-                        intersection = min(end, interval.end) - max(begin, interval.begin)
-                        union = max(end, interval.end) - min(begin, interval.begin)
-                        iou = intersection / union
-                        # if the gloss overlaps a lot with one in the intervalTree, skip gloss
-                        if iou >= 0.9:
-                            overlap_exceeded = True
-                            break
-                    if overlap_exceeded:
-                        return
+            clip_cnt += 1
 
-                if parsed_gloss in sb_vocab['gloss_to_id']:
-                    for interval in right_intervalTree:
-
-                        data = {'parsed_gloss': parsed_gloss}
-
-                        if begin == interval.end and interval.data['parsed_gloss'] == parsed_gloss:
-                            data['start_ms'] = interval.data['start_ms']
-                            data['stop_ms'] = stop_ms
-                            merged_interval = Interval(interval.begin, end, data)
-                            merged_intervalTree.remove(interval)
-                            merged_intervalTree.add(merged_interval)
-
-                        if end == interval.begin and interval.data == parsed_gloss:
-                            data['start_ms'] = start_ms
-                            data['stop_ms'] = interval.data['stop_ms']
-                            merged_interval = Interval(begin, interval.end, data)
-                            merged_intervalTree.remove(interval)
-                            merged_intervalTree.add(merged_interval)
-
-        clip_cnt = 0
-
-        for interval_obj in merged_intervalTree:
-            trimmed_filename = trim_clip(file_path,
-                                         interval_obj.data['start_ms'],
-                                         interval_obj.data['stop_ms'],
-                                         interval_obj.begin,
-                                         interval_obj.end,
-                                         interval_obj.data['parsed_gloss'],
-                                         sb_vocab['gloss_to_id'][interval_obj.data['parsed_gloss']],
-                                         cngt_output_root)
-
-            # if a trimmed file was created, save the corresponding metadata
-            if trimmed_filename is not None:
-                # since opencv's number of frames is unreliable, we count the frames ourselves
-                num_trimmed_frames = count_video_frames(trimmed_filename)
-                # we create metadata that will be helpful for the loading
-                metadata = {"num_frames": num_trimmed_frames, "start_frames": []}
-                num_clips = math.ceil(num_trimmed_frames / window_size)
-                for j in range(num_clips):
-                    metadata["start_frames"].append(j * window_size)
-
-                save_gzip(metadata, trimmed_filename[:-3] + 'gzip')
-
-                clip_cnt += 1
-
-        print(f"The file {file_path} generated {clip_cnt} clips")
+    print(f"The file {file_path} generated {clip_cnt} clips")
 
 def main(params):
     root = params.root
@@ -204,6 +203,7 @@ def main(params):
     cngt_output_folder = params.cngt_output_folder
     signbank_vocab_file = params.signbank_vocab_file
     window_size = params.window_size
+    framerate = params.framerate
 
     cngt_root = os.path.join(root, cngt_folder)
     cngt_output_root = os.path.join(root, cngt_output_folder)
@@ -259,6 +259,12 @@ if __name__ == "__main__":
         "--signbank_vocab_file",
         type=str,
         default="signbank_vocab.gzip"
+    )
+
+    parser.add_argument(
+        "--framerate",
+        type=int,
+        default="25"
     )
 
     parser.add_argument(
