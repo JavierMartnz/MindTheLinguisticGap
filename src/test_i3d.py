b6598eecb489b3dbbfc1c05437c3bf516b1eb533
Javier Martinez <javimr98@gmail.com>
2023-02-14 09:25:15 +0100

    Update test_i3d.py

--- src/test_i3d.py
+++ src/test_i3d.py
@@ -152,8 +152,10 @@ def test(cfg_path, log_filename, mode="rgb"):
 
     print(f"Predicting for glosses {glosses} mapped as classes {list(dataset.class_encodings.values())}")
 
-    i3d = InceptionI3d(num_classes=len(dataset.class_encodings), in_channels=3, window_size=16, input_size=cropped_input_size)
-    i3d.replace_logits(len(dataset.class_encodings))
+    i3d = InceptionI3d(num_classes=len(dataset.class_encodings),
+                       in_channels=3,
+                       window_size=window_size,
+                       input_size=cropped_input_size)
 
     if use_cuda:
         i3d.load_state_dict(torch.load(os.path.join(model_dir, run_dir, ckpt_filename)))
