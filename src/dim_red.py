c2342627bdd0a5ad4822a58c1361cab19ee9ee6f
Javier Martinez <javimr98@gmail.com>
2023-02-14 12:02:59 +0100

    Update dim_red.py

--- src/dim_red.py
+++ src/dim_red.py
@@ -156,8 +156,8 @@ def main(params):
     plt.style.use(Path(__file__).parent.resolve() / "../plot_style.txt")
 
     plt.plot(n_components, pca_stress)
-    plt.xticks(n_components)
     plt.xscale('linear')
+    plt.xticks(n_components)
     plt.xlabel("Number of dimensions")
     plt.ylabel("Stress")
     plt.tight_layout()
