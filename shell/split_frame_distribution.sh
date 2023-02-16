1cac65a231c423d597b0305a5f75bf85ebadc642
Javier Martinez <javimr98@gmail.com>
2023-02-16 12:20:29 +0100

    fd

--- shell/split_frame_distribution.sh
+++ shell/split_frame_distribution.sh
@@ -2,5 +2,5 @@
 
 source /vol/tensusers5/jmartinez/pretraini3d/bin/activate
 cd /vol/tensusers5/jmartinez/MindTheLinguisticGap
-python3 ./src/get_split_frame_distributions.py --root=/vol/tensusers5/jmartinez/datasets --cngt_folder=cngt_single_signs_12fps --sb_folder=NGT_Signbank_12fps --sb_vocab_file=signbank_vocab.gzip --fig_output_root=/vol/tensusers5/jmartinez/graphs
+python3 ./src/get_split_frame_distributions.py --root=/vol/tensusers5/jmartinez/datasets --cngt_folder=cngt_single_signs --sb_folder=NGT_Signbank_256 --sb_vocab_file=signbank_vocab.gzip --fig_output_root=/vol/tensusers5/jmartinez/graphs
 deactivate
