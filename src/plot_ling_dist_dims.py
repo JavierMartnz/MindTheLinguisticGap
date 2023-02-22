0bad1d7f0b4a019ecde647d35be4a597bef0c8af
Javier Martinez <javimr98@gmail.com>
2023-02-22 17:17:44 +0100

    ffv

--- src/plot_ling_dist_dims.py
+++ src/plot_ling_dist_dims.py
@@ -15,19 +15,27 @@ from src.utils.helpers import load_config
 def main():
 
     ling_dist = np.arange(1, 11)
+    n_dims = 2 ** np.arange(6, 11)
     min_num_dims = [128, 128, 128, 128, 128, 128, 128, 128, 128, 128]
-    performance = [0.7, 0.8, 0.8, 0.7, 0.8, 0.9, 0.7, 0.8, 0.85, 0.92]
+    acc = [0.6824, 0.7326, 0.5357, 0.7683, 0.6049, 0.7849, 0.6837, 0.8222, 0.7500, 0.9130]
+    f1 = [0.7033, 0.7677, 0.5063, 0.7467, 0.6596, 0.8113, 0.6931, 0.8298, 0.7800, 0.9259]
 
+    colors = sns.color_palette('pastel')
     plt.style.use(Path(__file__).parent.resolve() / "../plot_style.txt")
 
     fig, ax1 = plt.subplots()
-    ax1.plot(ling_dist, min_num_dims, linestyle='-', marker='o')
-    ax1.set_xlabel("Pair-wise linguistic distance")
+
+    ax1.set_xlabel('Linguistic distance')
     ax1.set_ylabel("Min number of dimensions")
+    ax1.plot(ling_dist, min_num_dims, linestyle='-', marker='o', color=colors[0])
+    ax1.tick_params(axis='y', labelcolor=colors[0])
+    ax1.set_yticks(n_dims)
 
     ax2 = ax1.twinx()
-    ax2.plot(ling_dist, performance, linestyle='--', marker='o')
     ax2.set_ylabel("Performance")
+    ax2.plot(ling_dist, acc, linestyle='--', label='accuracy', marker='o')
+    ax2.plot(ling_dist, f1, linestyle='--', label='f1', marker='o')
+    ax2.legend(loc='best')
 
     plt.tight_layout()
     plt.show()
