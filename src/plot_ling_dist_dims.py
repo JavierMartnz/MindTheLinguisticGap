6b09211e24b303ab86630e3abebb7c2b367028a4
Javier Martinez <javimr98@gmail.com>
2023-02-22 14:56:02 +0100

    Update plot_ling_dist_dims.py

--- src/plot_ling_dist_dims.py
+++ src/plot_ling_dist_dims.py
@@ -15,32 +15,22 @@ from src.utils.helpers import load_config
 def main():
 
     ling_dist = np.arange(1, 11)
-    min_num_dims = [128, 128, ]
-    performance = []
+    min_num_dims = [128, 128, 128, 128, 128, 128, 128, 128, 128, 128]
+    performance = [0.7, 0.8, 0.8, 0.7, 0.8, 0.9, 0.7, 0.8, 0.85, 0.92]
 
     plt.style.use(Path(__file__).parent.resolve() / "../plot_style.txt")
 
-    plt.plot(ling_dist, min_num_dims, label='dimesnison', linestyle='--')
-    plt.plot(epochs, training_history['train_f1'], label='train_f1', linestyle='--')
-    plt.plot(epochs, training_history['val_accuracy'], label='val_acc', linestyle='-')
-    plt.plot(epochs, training_history['val_f1'], label='val_f1', linestyle='-')
-    plt.legend()
+    fig, ax1 = plt.subplots()
+    ax1.plot(ling_dist, min_num_dims, linestyle='-', marker='o')
+    ax1.set_xlabel("Pair-wise linguistic distance")
+    ax1.set_ylabel("Min number of dimensions")
+
+    ax2 = ax1.twinx()
+    ax2.plot(ling_dist, performance, linestyle='--', marker='o')
+    ax2.set_ylabel("Performance")
+
     plt.tight_layout()
-    # plt.show()
-
-    os.makedirs(fig_output_root, exist_ok=True)
-    plt.savefig(os.path.join(fig_output_root, weights_root + '_metrics.png'))
-
-    plt.clf()
-
-    plt.plot(epochs, training_history['train_loss'], label='train_loss', linestyle='--')
-    plt.plot(epochs, training_history['val_loss'], label='val_loss', linestyle='-')
-    plt.legend()
-    plt.tight_layout()
-    # plt.show()
-
-    os.makedirs(fig_output_root, exist_ok=True)
-    plt.savefig(os.path.join(fig_output_root, weights_root + '_loss.png'))
+    plt.show()
 
 
 if __name__ == "__main__":
