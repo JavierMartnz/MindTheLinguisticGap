03a2bd7c46d3861fa10e4e25fc2d8fb845c3c484
Javier Martinez <javimr98@gmail.com>
2023-03-02 13:05:03 +0100

    Update plot_training_history.sh

--- shell/plot_training_history.sh
+++ shell/plot_training_history.sh
@@ -2,5 +2,5 @@
 
 source /vol/tensusers5/jmartinez/pretraini3d/bin/activate
 cd /vol/tensusers5/jmartinez/MindTheLinguisticGap
-python3 ./src/plot_training_history.py --trained_models_root=/vol/tensusers5/jmartinez/models/i3d --fig_output_root=/vol/tensusers5/jmartinez/graphs --config_path=/vol/tensusers5/jmartinez/MindTheLinguisticGap/config.yaml
+python3 ./src/plot_training_history.py --trained_models_root=/vol/tensusers5/jmartinez/models/i3d --fig_output_root=/vol/tensusers5/jmartinez/graphs --config_path=/vol/tensusers5/jmartinez/MindTheLinguisticGap/plot_history_config.yaml
 deactivate
