a5d5a31ced4625a6dc2779c598a81eae098b6789
Javier Martinez <javimr98@gmail.com>
2023-03-10 09:33:41 +0100

    Update autoencoder.py

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -41,66 +41,66 @@ class AutoEncoder(nn.Module):
         if k >= 1024:
             raise ValueError("The bottleneck layer has to have dimension k < 1024")
 
-        # self.encoder = nn.Sequential(
-        #     nn.Linear(1024, 512),
-        #     nn.ReLU(),
-        #     nn.Linear(512, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, 128),
-        #     nn.ReLU(),
-        #     nn.Linear(128, 64),
-        #     nn.ReLU(),
-        #     nn.Linear(64, 32),
-        #     nn.ReLU(),
-        #     nn.Linear(32, 16),
-        #     nn.ReLU(),
-        #     nn.Linear(16, 8),
-        #     nn.ReLU(),
-        #     nn.Linear(8, 4),
-        #     nn.ReLU(),
-        #     nn.Linear(4, 2),
-        # )
-        #
-        # self.decoder = nn.Sequential(
-        #     nn.Linear(2, 4),
-        #     nn.ReLU(),
-        #     nn.Linear(4, 8),
-        #     nn.ReLU(),
-        #     nn.Linear(8, 16),
-        #     nn.ReLU(),
-        #     nn.Linear(16, 32),
-        #     nn.ReLU(),
-        #     nn.Linear(32, 64),
-        #     nn.ReLU(),
-        #     nn.Linear(64, 128),
-        #     nn.ReLU(),
-        #     nn.Linear(128, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, 512),
-        #     nn.ReLU(),
-        #     nn.Linear(512, 1024),
-        #     nn.Sigmoid()
-        # )
+        self.encoder = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
+            nn.ReLU(),
+            nn.Linear(4, 2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(2, 4),
+            nn.ReLU(),
+            nn.Linear(4, 8),
+            nn.ReLU(),
+            nn.Linear(8, 16),
+            nn.ReLU(),
+            nn.Linear(16, 32),
+            nn.ReLU(),
+            nn.Linear(32, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1024),
+            nn.Sigmoid()
+        )
 
         # the autoencoder always has 4 layers, so calculate their input/output dimensions based on the given k
-        dim_step = (1024 - self.k) // self.n_layers
-        decoder_dims = list(range(self.k, 1024, dim_step))
-        decoder_dims.insert(len(decoder_dims), 1024)
-        encoder_dims = list(reversed(decoder_dims))
-
-        self.encoder = nn.ModuleList()
-        for idx in range(len(encoder_dims) - 1):
-            self.encoder.append(nn.Linear(encoder_dims[idx], encoder_dims[idx + 1]))
-            if idx + 1 < len(encoder_dims) - 1:  # if not the last layer
-                self.encoder.append(nn.ReLU())
-
-        self.decoder = nn.ModuleList()
-        for idx in range(len(decoder_dims) - 1):
-            self.decoder.append(nn.Linear(decoder_dims[idx], decoder_dims[idx + 1]))
-            if idx + 1 < len(decoder_dims) - 1:
-                self.decoder.append(nn.ReLU())
-            else:  # if last layer
-                self.decoder.append(nn.Sigmoid())
+        # dim_step = (1024 - self.k) // self.n_layers
+        # decoder_dims = list(range(self.k, 1024, dim_step))
+        # decoder_dims.insert(len(decoder_dims), 1024)
+        # encoder_dims = list(reversed(decoder_dims))
+        #
+        # self.encoder = nn.ModuleList()
+        # for idx in range(len(encoder_dims) - 1):
+        #     self.encoder.append(nn.Linear(encoder_dims[idx], encoder_dims[idx + 1]))
+        #     if idx + 1 < len(encoder_dims) - 1:  # if not the last layer
+        #         self.encoder.append(nn.ReLU())
+        #
+        # self.decoder = nn.ModuleList()
+        # for idx in range(len(decoder_dims) - 1):
+        #     self.decoder.append(nn.Linear(decoder_dims[idx], decoder_dims[idx + 1]))
+        #     if idx + 1 < len(decoder_dims) - 1:
+        #         self.decoder.append(nn.ReLU())
+        #     else:  # if last layer
+        #         self.decoder.append(nn.Sigmoid())
 
     def forward(self, x):
         # x = self.encoder(x)
@@ -159,6 +159,116 @@ def plot_training_history(config: dict, training_history: dict, fig_output_root:
 
     plt.savefig(os.path.join(fig_output_root, filename + '_metrics.png'))
 
+
+def train_trimmed_autoencoder(config, dataloaders, fig_output_root, log_file):
+    train_config = config.get("training")
+
+    lr = train_config.get("lr")
+    weight_decay = train_config.get("weight_decay")
+    epochs = train_config.get("epochs")
+    n_layers = train_config.get("n_layers")
+    use_cuda = train_config.get("use_cuda")
+    ks = train_config.get("ks")
+
+    # initialize autoencoder
+    autoencoder = AutoEncoder(n_layers=n_layers, k=ks[0])
+    if use_cuda:
+        autoencoder.cuda()
+
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(autoencoder.parameters(), lr=lr, weight_decay=weight_decay)
+
+    # early stopping setup
+    min_delta = 0.0
+    patience = 10
+    early_stopper = EarlyStopper(patience=patience, min_delta=min_delta)
+
+    lr_sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True, threshold=min_delta)
+
+    training_history = {"loss": {"train": [], "val": []},
+                        "metric": {"train_mse": [], "val_mse": []}}
+
+    min_loss = np.inf
+    early_stop_flag = False
+
+    print(f"Training autoencoder with {n_layers} layers and k={k}...")
+    for epoch in range(epochs):
+        if early_stop_flag:
+            print(f"Early stopping: validation loss did not decrease more than {early_stopper.min_delta} in {early_stopper.patience} epochs.")
+            break
+        for phase in ["train", "val"]:
+            tot_loss = 0.0
+            num_iter = 0
+            epoch_mse = []
+
+            autoencoder.train(True) if phase == "train" else autoencoder.train(False)
+
+            for data in dataloaders[phase]:
+                num_iter += 1
+                optimizer.zero_grad()
+
+                feature_vector = Variable(data.cuda())
+                preds = autoencoder(feature_vector)
+
+                epoch_mse.append(mean_squared_error(y_true=feature_vector.detach().cpu().numpy(), y_pred=preds.detach().cpu().numpy()))
+
+                loss = criterion(preds, feature_vector)
+                loss.backward()
+                tot_loss += loss.item()
+
+                if phase == 'train':
+                    optimizer.step()
+
+            if phase == "train":
+                training_history["loss"]["train"].append(tot_loss / num_iter)
+                training_history["metric"]["train_mse"].append(np.mean(epoch_mse))
+
+                # store the model state_dict to store it later if the val loss improves
+                train_ckpt = autoencoder.state_dict()
+
+            if phase == "val":
+                training_history["loss"]["val"].append(tot_loss / num_iter)
+                training_history["metric"]["val_mse"].append(np.mean(epoch_mse))
+
+                early_stop_flag = early_stopper(tot_loss / num_iter)
+                if (tot_loss / num_iter) < min_loss:
+                    min_loss = tot_loss / num_iter
+                    best_ckpt = train_ckpt
+                    best_epoch = epoch
+
+                lr_sched.step(tot_loss / num_iter)
+
+    plot_training_history(config, training_history, fig_output_root)
+
+    # make sure we use the best checkpoint
+    autoencoder.load_state_dict(best_ckpt)
+    autoencoder.train(False)
+
+    trimmed_autoencoder = autoencoder
+
+    ks = [2, 4, 8, 16, 32, 64, 128, 256, 512]
+
+    for i in range(len(ks)):
+        # only remove autoencoder layers from the second iteration
+        if i > 0:
+            # remove last two layers (linear + relu) of the encoder
+            trimmed_autoencoder.encoder = nn.Sequential(*list(trimmed_autoencoder.encoder.children())[:-2])
+            # remove first two layers (linear + relu) of the decoder
+            trimmed_autoencoder.decoder = nn.Sequential(*list(trimmed_autoencoder.decoder.children())[2:])
+
+        # summary(trimmed_autoencoder, (1, 1024))
+        total_true = []
+        total_pred = []
+        for feature_vector in dataloaders["train"]:
+            feature_vector = Variable(feature_vector.cuda())
+            preds = autoencoder(feature_vector)
+            total_true.extend(feature_vector.detach().cpu().numpy())
+            total_pred.extend(preds.detach().cpu().numpy())
+
+            # print to console
+            print(f"Autoencoder with k={ks[i]}, test MSE={mean_squared_error(y_true=total_true, y_pred=total_pred):.6f}")
+            # print to file
+            print(f"Autoencoder with k={ks[i]}, test MSE={mean_squared_error(y_true=total_true, y_pred=total_pred):.6f}", file=log_file)
 
 def train_autoencoder(config: dict, dataloaders: dict, k: int, fig_output_root: str, log_file: str):
     train_config = config.get("training")
@@ -357,8 +467,6 @@ def train(config: dict, fig_output_root: str, log_output_root: str):
 
     i3d.train(False)  # Set model to evaluate mode
 
-
-
     print(f"Running datapoints through model...")
     with torch.no_grad():  # this deactivates gradient calculations, reducing memory consumption by A LOT
         for phase in ["train", "val"]:
@@ -397,9 +505,12 @@ def train(config: dict, fig_output_root: str, log_output_root: str):
     dataloaders["train"] = torch.utils.data.DataLoader(train_features, batch_size=batch_size, shuffle=True, num_workers=0)
     dataloaders["val"] = torch.utils.data.DataLoader(val_features, batch_size=batch_size, shuffle=True, num_workers=0)
 
-    with open(os.path.join(log_output_root, "autoencoder_test_log.txt"), "w") as file:
-        for k in ks:
-            train_autoencoder(config, dataloaders, k, fig_output_root, log_file=file)
+    with open(os.path.join(log_output_root, "autoencoder_trimmed_test_log.txt"), "w") as file:
+        train_trimmed_autoencoder(config, dataloaders, fig_output_root, log_file=file)
+
+    # with open(os.path.join(log_output_root, "autoencoder_test_log.txt"), "w") as file:
+    #     for k in ks:
+    #         train_autoencoder(config, dataloaders, k, fig_output_root, log_file=file)
 
 
 def main(params):
