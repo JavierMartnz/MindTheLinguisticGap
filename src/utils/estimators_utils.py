70b1b17b1488b1e056c0942890d4dd23dc665e1b
Javier Martinez <javimr98@gmail.com>
2023-03-22 18:41:13 +0100

    fix

---
+++ src/utils/estimators_utils.py
@@ -0,0 +1,172 @@
+import torch
+from torch import nn
+from torch.utils.data.sampler import Sampler
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def get_curvature(num_samples, dists, fig_name="curvature"):
+    N = np.log(num_samples)
+    eps = np.log(dists)
+    m, b = np.polyfit(eps, N, 1)
+    print(m, b)
+    fig = plt.figure()
+    plt.scatter(eps, N)
+    fig.suptitle('CIFAR10', fontsize=12)
+    plt.xlabel('log(eps)', fontsize=12)
+    plt.ylabel('log(N)', fontsize=12)
+    plt.savefig("{}_m{}_b{}.pdf".format(fig_name, m, b))
+    # plt.show()
+
+
+class KNNComputer(nn.Module):
+    """
+    Using this hack for data parallel
+    """
+    def __init__(self, sample_num, K=1):
+        super(KNNComputer, self).__init__()
+
+        self.K = K
+        self.register_buffer("num_computed", torch.zeros([]))
+        if K == 1:
+            self.register_buffer("min_dists", torch.full((sample_num, ), float('inf')))
+        else:
+            self.register_buffer("min_dists", torch.full((sample_num, K), float('inf')))
+
+    def forward(self, x, x_idx_start, y, y_idx_start):
+        # update the min dist for existing examples...
+        x_bsize, y_bsize = x.size(0), y.size(0)
+        x = x.view(x_bsize, -1)
+        y = y.view(y_bsize, -1)
+        # dist = torch.norm(x.unsqueeze(1) - y.unsqueeze(0), dim=2)
+        dist = torch.cdist(x, y, p=2, compute_mode="donot_use_mm_for_euclid_dist")
+
+        # need to ignore the distance to the data point itself
+        x_idxes = torch.Tensor([i for i in range(x_idx_start, x_idx_start+x_bsize)]).view(-1, 1).to(x)
+        y_idxes = torch.Tensor([i for i in range(y_idx_start, y_idx_start+y_bsize)]).view(1, -1).to(x)
+        ignore_idx_mask = x_idxes == y_idxes
+        if torch.any(ignore_idx_mask):
+            dist[ignore_idx_mask] = float('inf')
+
+        if self.K == 1:
+            new_min_dist, _ = torch.min(dist, dim=1)
+
+            self.min_dists[x_idx_start:x_idx_start+x_bsize] = torch.min(new_min_dist,
+                                                                  self.min_dists[x_idx_start:x_idx_start+x_bsize])
+        else:
+            comp = torch.cat([dist, self.min_dists[x_idx_start:x_idx_start+x_bsize]], dim=1)
+            updated_min_dist, _ = torch.topk(comp, self.K, dim=1, largest=False)
+            self.min_dists[x_idx_start:x_idx_start + x_bsize] = updated_min_dist
+
+    def get_mean_nn_dist(self, sidx, eidx):
+        if self.K == 1:
+            return torch.mean(self.min_dists[sidx:eidx])
+
+
+class KNNComputerNoCheck(nn.Module):
+    """
+    Using this hack for data parallel
+    without checking for the sample itself
+    """
+    def __init__(self, sample_num, K=1, cosine_dist=False):
+        super(KNNComputerNoCheck, self).__init__()
+
+        self.K = K
+        self.cosine_dist = cosine_dist
+        self.register_buffer("num_computed", torch.zeros([]))
+
+        if K == 1:
+            self.register_buffer("min_dists", torch.full((sample_num, ), float('inf')))
+            self.register_buffer("nn_indices", torch.full((sample_num,), 0, dtype=torch.int64))
+        else:
+            self.register_buffer("min_dists", torch.full((sample_num, K), float('inf')))
+            self.register_buffer("nn_indices", torch.full((sample_num, K), 0, dtype=torch.int64))
+
+    def forward(self, x, x_idx_start, y, y_idx_start):
+        # update the min dist for existing examples...
+        x_bsize, y_bsize = x.size(0), y.size(0)
+        x = x.view(x_bsize, -1)
+        y = y.view(y_bsize, -1)
+        if self.cosine_dist:
+
+            x = x / x.norm(dim=1, keepdim=True)
+            y = y / y.norm(dim=1, keepdim=True)
+            dist = x.mm(y.t())
+
+        else:
+            # dist = torch.norm(x.unsqueeze(1) - y.unsqueeze(0), dim=2)
+            dist = torch.cdist(x, y, p=2, compute_mode="donot_use_mm_for_euclid_dist")
+
+        if self.K == 1:
+            new_min_dist, nn_idxes = torch.min(dist, dim=1)
+
+            self.min_dists[x_idx_start:x_idx_start + x_bsize] = torch.min(new_min_dist,
+                                                                  self.min_dists[x_idx_start:x_idx_start+x_bsize])
+
+            self.nn_indices[x_idx_start:x_idx_start + x_bsize] = nn_idxes + y_idx_start
+        else:
+            comp = torch.cat([dist, self.min_dists[x_idx_start:x_idx_start+x_bsize]], dim=1)
+            # updated_min_dist, nn_idxes = torch.topk(comp, self.K, dim=1, largest=False)
+            # check for repeated images
+            sorted_dists, sorted_idxes = torch.sort(comp, dim=1, descending=False)
+            updated_dist_list, nn_idx_list = [], []
+            for row in range(sorted_dists.shape[0]):
+                sidx = 1
+                while sidx < sorted_dists.shape[1]:
+                    if sorted_dists[row, sidx] == 0:
+                        sidx += 1
+                    else:
+                        break
+                updated_dist_list.append(sorted_dists[row, sidx-1:sidx-1+self.K])
+                nn_idx_list.append(sorted_idxes[row, sidx-1:sidx-1+self.K])
+            updated_min_dist = torch.stack(updated_dist_list)
+            nn_idxes = torch.stack(nn_idx_list)
+
+            self.min_dists[x_idx_start:x_idx_start + x_bsize] = updated_min_dist
+
+            sample_idxes = (nn_idxes < y_bsize).int() * (nn_idxes + y_idx_start) \
+                           + (nn_idxes >= y_bsize).int() * self.nn_indices[x_idx_start:x_idx_start + x_bsize]
+            self.nn_indices[x_idx_start:x_idx_start + x_bsize] = sample_idxes
+
+    def get_mean_nn_dist(self, sidx, eidx):
+        if self.K == 1:
+            return torch.mean(self.min_dists[sidx:eidx])
+
+
+def update_nn(anchor_loader, anchor_start_idx, new_img_loader, new_start_idx, nn_computer):
+    anchor_counter = anchor_start_idx
+    # ignoring the labels
+    with torch.no_grad():
+        for n, (abatch, _) in enumerate(anchor_loader):
+            abatch = abatch.cuda()
+
+            new_img_counter = new_start_idx
+            for newbatch, _ in new_img_loader:
+                newbatch = newbatch.cuda()
+
+                nn_computer(abatch, anchor_counter, newbatch, new_img_counter)
+
+                new_img_counter += newbatch.size(0)
+
+                equiv_flag = (nn_computer.min_dists[anchor_start_idx:anchor_start_idx+abatch.size(0), 0] == 0) & (nn_computer.min_dists[anchor_start_idx:anchor_start_idx+abatch.size(0), 1] == 0)
+                if torch.any(equiv_flag):
+                    raise Exception("Identical data detected!")
+
+            anchor_counter += abatch.size(0)
+
+            #if n % 50 == 0 or n == len(anchor_loader) - 1:
+            #    #print("Finished {} images".format(anchor_counter))
+
+
+def create_random_subsets(data_set, subset_size):
+    indices = [i for i in range(len(data_set))]
+    random.shuffle(indices)
+
+    n_subsets = len(data_set) // subset_size
+    subset_idxes = [indices[i*subset_size: (i+1)*subset_size] for i in range(n_subsets)]
+
+    return [torch.utils.data.Subset(data_set, sidxes) for sidxes in subset_idxes], indices
--- src/utils/geomle.py
+++ src/utils/geomle.py
@@ -1,8 +1,60 @@
-import pandas as pd
+"""
+This file modified from:
+* https://github.com/stat-ml/GeoMLE/blob/master/geomle/geomle.py
+"""
+
+import sys
+sys.path.append("/vol/tensusers5/jmartinez/MindTheLinguisticGap")
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
-from sklearn.linear_model import LinearRegression, Ridge
+import pandas as pd
 from functools import partial
+import torch
+from src.utils.estimators_utils import KNNComputerNoCheck, update_nn
+from sklearn.linear_model import LinearRegression, Ridge
+
+
+def mle_center(k=5, dist=None):
+    """
+    Returns Levina-Bickel dimensionality estimation
+    Input parameters:
+    X        - data points
+    X_center - data centers
+    k        - number of nearest neighbours (Default = 5)
+    dist     - matrix of distances to the k nearest neighbors of each point (Optional)
+    Returns:
+    dimensionality estimation for the k
+    """
+    dist = dist[:, 0:k]
+    assert np.all(dist > 0)
+    d = np.log(dist[:, k - 1: k] / dist[:, 0:k - 1])
+    d = d.sum(axis=1) / (k - 2)
+    d = 1. / d
+    intdim_sample = d
+    Rs = dist[:, -1]
+
+    return intdim_sample, Rs
+
+
+def intrinsic_dim_sample_wise_double_mle(k=5, dist=None):
+    """
+    Returns Levina-Bickel dimensionality estimation and the correction by MacKay-Ghahramani
+    Input parameters:
+    X    - data
+    k    - number of nearest neighbours (Default = 5)
+    dist - matrix of distances to the k (or more) nearest neighbors of each point (Optional)
+    Returns:
+    two dimensionality estimates
+    """
+    dist = dist[:, 0:k]
+    assert np.all(dist > 0)
+    d = np.log(dist[:, k - 1: k] / dist[:, 0:k - 1])
+    d = d.sum(axis=1) / (k - 2)
+    inv_mle = d.copy()
+    d = 1. / d
+    mle = d
+    Rs = dist[:, -1]
+    inv_Rs = 1. / dist[:, -1]
+    return mle, inv_mle, Rs, inv_Rs
 
 
 def tolist(x):
@@ -13,50 +65,9 @@ def tolist(x):
     if type(x) == np.ndarray:
         return x.tolist()
 
-
-def drop_zero_values(dist):
-    mask = dist[:, 0] == 0
-    dist[mask] = np.hstack([dist[mask][:, 1:], dist[mask][:, 0:1]])
-    dist = dist[:, :-1]
-    assert np.all(dist > 0)
-    return dist
-
-
-def mle_center(X, X_center, k=5, dist=None):
-    """
-    Returns Levina-Bickel dimensionality estimation
-
-    Input parameters:
-    X        - data points
-    X_center - data centers
-    k        - number of nearest neighbours (Default = 5)
-    dist     - matrix of distances to the k nearest neighbors of each point (Optional)
-
-    Returns:
-    dimensionality estimation for the k
-    """
-    if len(X_center.shape) != 2:
-        X_center = X_center.values.reshape(1, -1)
-    if dist is None:
-        neighb = NearestNeighbors(n_neighbors=k + 1, n_jobs=1,
-                                  algorithm='ball_tree').fit(X)
-        dist, ind = neighb.kneighbors(X_center)
-        dist = drop_zero_values(dist)
-    dist = dist[:, 0:k]
-    assert dist.shape == (X_center.shape[0], k)
-    assert np.all(dist > 0)
-    d = np.log(dist[:, k - 1: k] / dist[:, 0:k - 1])
-    d = d.sum(axis=1) / (k - 2)
-    d = 1. / d
-    intdim_sample = d
-    Rs = dist[:, -1]
-    return intdim_sample, Rs
-
-
 def fit_poly_reg(x, y, w=None, degree=(1, 2), alpha=5e-3):
     """
     Fit regression and return f(0)
-
     Input parameters:
     x - features (1d-array)
     y - targets (1d-array)
@@ -72,20 +83,31 @@ def fit_poly_reg(x, y, w=None, degree=(1, 2), alpha=5e-3):
     return lm.intercept_
 
 
-def _func(df, degree, alpha):
+def _func(df, degree, alpha, inv_mle=False):
     gr_df = df.groupby('k')
-    R = gr_df['R'].mean().values
-    d = gr_df['dim'].mean().values
-    std = gr_df['dim'].std().values
+    if inv_mle:
+        d = gr_df['inv_mle_dim'].mean().values
+        std = gr_df['inv_mle_dim'].std().values
+        R = gr_df['inv_mle_R'].mean().values
+    else:
+        d = gr_df['dim'].mean().values
+        std = gr_df['dim'].std().values
+        R = gr_df['R'].mean().values
     if np.isnan(std).any(): std = np.ones_like(std)
-    return fit_poly_reg(R, d, std ** -1, degree=degree, alpha=alpha)
+    return fit_poly_reg(R, d, std**-1, degree=degree, alpha=alpha)
+
+def drop_zero_values(dist):
+    mask = dist[:,0] == 0
+    dist[mask] = np.hstack([dist[mask][:, 1:], dist[mask][:,0:1]])
+    dist = dist[:, :-1]
+    assert np.all(dist > 0)
+    return dist
 
 
-def geomle(X, k1=10, k2=40, nb_iter1=10, nb_iter2=20, degree=(1, 2),
-           alpha=5e-3, ver='GeoMLE', random_state=None, debug=False):
+def geomle(full_dataset, k1=10, k2=40, nb_iter1=10, nb_iter2=20, degree=(1, 2),
+           alpha=5e-3, ver='GeoMLE', random_state=None, debug=False, args=None):
     """
     Returns range of Levina-Bickel dimensionality estimation for k = k1..k2 (k1 < k2) averaged over bootstrap samples
-
     Input parameters:
     X            - data
     k1           - minimal number of nearest neighbours (Default = 10)
@@ -95,7 +117,6 @@ def geomle(X, k1=10, k2=40, nb_iter1=10, nb_iter2=20, degree=(1, 2),
     degree       - (Default = (1, 2))
     alpha        - (Default = 5e-3)
     random_state - random state (Optional)
-
     Returns:
     array of shape (nb_iter1,) of regression dimensionality estimation for k = k1..k2 averaged over bootstrap samples
     """
@@ -103,41 +124,69 @@ def geomle(X, k1=10, k2=40, nb_iter1=10, nb_iter2=20, degree=(1, 2),
         rng = np.random
     else:
         rng = np.random.RandomState(random_state)
-    nb_examples = X.shape[0]
-    dim_space = X.shape[1]
+    nb_examples = len(full_dataset)
+    dim_space = float("inf")
 
     result = []
     data_reg = []
     for i in range(nb_iter1):
         dim_all, R_all, k_all, idx_all = [], [], [], []
+        inv_dim_all = []
+        inv_R_all = []
         for j in range(nb_iter2):
             idx = np.unique(rng.randint(0, nb_examples - 1, size=nb_examples))
-            X_bootstrap = X.iloc[idx]
-            neighb = NearestNeighbors(n_neighbors=k2 + 1, n_jobs=1,
-                                      algorithm='ball_tree').fit(X_bootstrap)
-            dist, ind = neighb.kneighbors(X)
+            X_bootstrap = torch.utils.data.Subset(full_dataset, idx)
+            #print("Bootstrap round {} with {} samples".format(j, len(X_bootstrap)))
+            # compute the KNN with pytorch
+            nn_computer = KNNComputerNoCheck(len(full_dataset), K=k2+1).cuda()
+
+            anchor_loader = torch.utils.data.DataLoader(full_dataset,
+                                                        batch_size=args.bsize, shuffle=False,
+                                                        num_workers=args.n_workers)
+            bootstrap_loader = torch.utils.data.DataLoader(X_bootstrap,
+                                                        batch_size=args.bsize, shuffle=False,
+                                                        num_workers=args.n_workers)
+
+            update_nn(anchor_loader, 0, bootstrap_loader, 0, nn_computer)
+
+            dist = nn_computer.min_dists.cpu().numpy()
             dist = drop_zero_values(dist)
-            dist = dist[:, 0:k2]
+            dist = dist[:, :k2]
             assert np.all(dist > 0)
 
             for k in range(k1, k2 + 1):
-                dim, R = mle_center(X_bootstrap, X, k, dist)
+                # if args.inv_mle:
+                #     dim, inv_mle_dim, R, R_inv = intrinsic_dim_sample_wise_double_mle(k=k, dist=dist)
+                #     inv_dim_all += inv_mle_dim.tolist()
+                #     inv_R_all += R_inv.tolist()
+                # else:
+                dim, R = mle_center(k, dist)
                 dim_all += dim.tolist()
                 R_all += R.tolist()
                 idx_all += list(range(nb_examples))
                 k_all += [k] * dim.shape[0]
-
+            #print("Finished bootstrap {}".format(j))
         data = {'dim': dim_all,
                 'R': R_all,
                 'idx': idx_all,
                 'k': k_all}
+        # if args.inv_mle:
+        #     data['inv_mle_dim'] = inv_dim_all
+        #     data['inv_mle_R'] = inv_R_all
 
         df = pd.DataFrame(data)
-        if ver == 'GeoMLE':
-            func = partial(_func, degree=degree, alpha=alpha)
-            reg = df.groupby('idx').apply(func).values.mean()
+        if ver.lower() == 'GeoMLE'.lower():
+            func = partial(_func, degree=degree, alpha=alpha, inv_mle=False)
+            # if args.inv_mle:
+            #     reg = 1. / (1. / df.groupby('idx').apply(func).values).mean()
+            # else:
+            reg = df.groupby('idx').apply(func).values
+            if args.inv_mle:
+                reg = 1. / (1. / reg).mean()
+            else:
+                reg = reg.mean()
             data_reg.append(df)
-        elif ver == 'fastGeoMLE':
+        elif ver.lower() == 'fastGeoMLE'.lower():
             df_gr = df.groupby(['idx', 'k']).mean()[['R', 'dim']]
             R = df_gr.groupby('k').R.mean()
             d = df_gr.groupby('k').dim.mean()
@@ -147,7 +196,7 @@ def geomle(X, k1=10, k2=40, nb_iter1=10, nb_iter2=20, degree=(1, 2),
         else:
             assert False, 'Unknown mode {}'.format(ver)
         reg = 0 if reg < 0 else reg
-        reg = dim_space if reg > dim_space else reg
+        # reg = dim_space if reg > dim_space else reg
         result.append(reg)
     if debug:
         return np.array(result), data_reg
